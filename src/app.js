ad1f38652ef02be666d94eb105b7aeb70cd48352
CharlyCoderHouse <carlosdiblasi@gmail.com>
2023-04-25 20:32:37 -0300

    Chat finalizado

--- src/app.js
+++ src/app.js
@@ -23,3 +23,18 @@ app.use('/', viewsRouter)
 
 const server = app.listen(8080, () => console.log('Server running'));
 const io = new Server(server);
+
+const messages = [];
+
+io.on('connection', socket => {
+    console.log('Nuevo cliente conectado');
+    socket.on('message', data => {
+        messages.push(data);
+        io.emit('messageLogs', messages);
+    });
+
+    socket.on('autenticated', data => {
+        socket.emit('messageLogs', messages);
+        socket.broadcast.emit('newUserConnected', data);
+    });
+})
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -20,4 +20,34 @@ Swal.fire({
     allowEscapeKey: false
 }).then(result =>{
     user = result.value;
+    socket.emit('autenticated', user);
 }); 
+
+chatBox.addEventListener('keyup', evt =>{
+    if(evt.key==='Enter'){
+        if(chatBox.value.trim().length > 0){
+            socket.emit('message', { user, message: chatBox.value });
+            chatBox.value='';
+        }
+    }
+});
+
+socket.on('messageLogs', data =>{
+    let log = document.getElementById('messageLogs');
+    let messages = '';
+    data.forEach(message => {
+        messages += `${message.user} dice: ${message.message} <br/>`
+    });
+    log.innerHTML=messages;
+});
+
+socket.on('newUserConnected', data =>{
+    Swal.fire({
+        toast: true,
+        position: 'top-end',
+        showConfirmationButton: false,
+        timer: 3000,
+        title: `${data} se ha unido al chat`,
+        icon: 'success'
+    }); 
+});
